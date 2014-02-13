4ef96da83039a6659b975003115ec13b31b07965
Morgan Martinet <morgan.martinet@imag.fr>
2014-02-13 13:16:33 +0100

    Improve getFactory and GetHandlerFactory methods
    Default signatures now has a timeout of 10s and fail if no factory is found.
    Add two signatures for the both methods to parametrized the timeout and the fail.

--- src/main/java/org/ow2/chameleon/testing/helpers/FactoryHelper.java
+++ src/main/java/org/ow2/chameleon/testing/helpers/FactoryHelper.java
@@ -42,13 +42,30 @@ public class FactoryHelper extends AbstractHelper {
      * If timeout is set to 0, it sets the timeout to 10s.
      *
      * @param factoryName the name of the factory to retrieve.
+     * @param timeout     the timeout
      * @return the component factory with the given name in the local bundle, or
      *         {@code null} if not found.
      */
     public Factory getFactory(String factoryName, long timeout) {
         // Waits for the factory.
+        return getFactory(factoryName, timeout, true);
+    }
+
+    /**
+     * Returns the component factory with the given name.
+     * If the factory is not available, it adopts the timeout value given as parameter (in milliseconds).
+     * If timeout is set to 0, it sets the timeout to 10s.
+     *
+     * @param factoryName the name of the factory to retrieve.
+     * @param timeout     the timeout
+     * @param fail        fail the test if there are no factory when the timeout is reached
+     * @return the component factory with the given name in the local bundle, or
+     *         {@code null} if not found.
+     */
+    public Factory getFactory(String factoryName, long timeout, boolean fail) {
+        // Waits for the factory.
         return osgiHelper.waitForService(Factory.class,
-                "(factory.name=" + factoryName + ")", timeout, false);
+                "(factory.name=" + factoryName + ")", timeout, fail);
     }
 
     /**
@@ -65,14 +82,44 @@ public class FactoryHelper extends AbstractHelper {
 
     /**
      * Returns the handler factory with the given name in the local bundle.
+     * If the factory is not available, it waits for it 10  seconds.
      *
      * @param factoryName the name of the handler factory to retrieve.
      * @return the handler factory with the given name in the local bundle, or
      *         {@code null} if not found.
      */
     public HandlerFactory getHandlerFactory(String factoryName) {
+        return getHandlerFactory(factoryName, 0);
+    }
+
+    /**
+     * Returns the handler factory with the given name in the local bundle.
+     * If the factory is not available, it adopts the timeout value given as parameter (in milliseconds).
+     * If timeout is set to 0, it sets the timeout to 10s.
+     *
+     * @param factoryName the name of the handler factory to retrieve.
+     * @param timeout     the timeout
+     * @return the handler factory with the given name in the local bundle, or
+     *         {@code null} if not found.
+     */
+    public HandlerFactory getHandlerFactory(String factoryName, long timeout) {
+        return getHandlerFactory(factoryName, timeout, true);
+    }
+
+    /**
+     * Returns the handler factory with the given name in the local bundle.
+     * If the factory is not available, it adopts the timeout value given as parameter (in milliseconds).
+     * If timeout is set to 0, it sets the timeout to 10s.
+     *
+     * @param factoryName the name of the handler factory to retrieve.
+     * @param timeout     the timeout
+     * @param fail        fail the test if there are no factory when the timeout is reached
+     * @return the handler factory with the given name in the local bundle, or
+     *         {@code null} if not found.
+     */
+    public HandlerFactory getHandlerFactory(String factoryName, long timeout, boolean fail) {
         return osgiHelper.waitForService(HandlerFactory.class,
-                "(" + Handler.HANDLER_NAME_PROPERTY + "=" + factoryName + ")", 1000, false);
+                "(" + Handler.HANDLER_NAME_PROPERTY + "=" + factoryName + ")", timeout, fail);
     }
 
 }
